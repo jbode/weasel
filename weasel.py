c6b7aa56c1d5cc96066de69bcf8a26d6c9a5fae7
jbode <jfbode1029@gmail.com>
2017-09-25 13:50:10 -0500

    Added Python version of Weasel program
    Ported the C code to Python 2.4.

---
+++ weasel.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+import random
+import math
+import optparse
+
+default_alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.,!?'\" "
+default_target = "methinks it is like a weasel"
+default_size = 100
+default_length = 10
+
+default_delete = 1
+default_transpose = 1
+default_duplicate = 1
+default_point = 1
+
+alphabet = default_alphabet
+target = default_target
+
+delete = 100 - default_delete
+transpose  = delete - default_transpose
+duplicate = transpose - default_duplicate
+point = duplicate - default_point
+no_change = 95
+
+def rollDie():
+  return int( random.random() * 100 )
+
+def randomChar():
+  idx = int( random.random() * len( alphabet ) )
+  return alphabet[ idx ]
+
+def randomString( slen ):
+  r = []
+  for i in xrange( slen ):
+    r.append( randomChar() )
+  s = ''.join( r )
+  return s
+
+def mutate( src ):
+  tmp = []
+  for i in xrange( len( src ) ):
+    action = rollDie()
+    if action >= point and action < duplicate:
+      tmp.append( randomChar() )
+    elif action >= duplicate and action < transpose:
+      tmp.append( src[i] )
+      tmp.append( src[i] )
+    elif action >= transpose and action < delete:
+      if i < len( src ) - 1:
+        tmp.append( src[i+1] )
+      tmp.append( src[i] )
+    elif action < point:
+      tmp.append( src[i] )
+  return ''.join( tmp )
+
+def cmpWeasel( w0, w1 ):
+  return w0[2] - w1[2]
+
+def score( cand, targ ):
+  clen = len( cand )
+  tlen = len( targ )
+  s = 0
+  for i in xrange( min( clen, tlen ) ):
+    s += abs( ord( cand[i] ) - ord( targ[i] ) )
+  if clen < tlen:
+    for i in xrange( clen, tlen ):
+      s += ord( targ[i] )
+  if tlen < clen:
+    for i in xrange( tlen, clen ):
+      s += ord( cand[i] );
+  return s
+
+def main():
+  p = optparse.OptionParser()
+  p.add_option( '--target', '-T', default=default_target, 
+                help='string to match (default "' + default_target + '")' );
+  p.add_option( '--alphabet', '-A', default=default_alphabet, 
+                help='source alphabet (default "' + default_alphabet + '")' )
+  p.add_option( '--size', '-S', default=default_size, help='population size (default ' + str(default_size) + ')' )
+  p.add_option( '--point', '-p', default=default_point, help='chance of point change event (0 to 100, default ' + str(default_point) +')' )
+  p.add_option( '--duplicate', '-d', default=default_duplicate, help='chance of duplicate event (0 to 100, default ' + str(default_duplicate) + ')' )
+  p.add_option( '--delete', '-x', default=default_delete, help='chance of delete event (0 to 100, default ' + str(default_delete) + ')' )
+  p.add_option( '--transpose', '-t', default=default_transpose, help='chance of transpose event (0 to 100, default ' + str(default_transpose) + ')' )
+  p.add_option( '--length', '-l', default=default_length, help='initial string size (default ' + str(default_length) + ')' )
+
+  options, arguments = p.parse_args()
+
+  alphabet = options.alphabet
+  target = options.target
+
+  delete = 100 - int(options.delete)
+  transpose = delete - int(options.transpose)
+  duplicate = transpose - int(options.duplicate)
+  point = duplicate - int(options.point)
+
+  population = []
+  generation = 0
+
+  for i in xrange(int(options.size)):
+    candidate = randomString( int(options.length) )
+    population.append( (generation, candidate, score(candidate, target)) )
+
+  population.sort( cmpWeasel )
+
+  print "%15s%15s%8s%s" % ("Generation", "Score", " ", "Value")
+  print "%15s%15s%8s%s" % ("----------", "-----", " ", "-----")
+
+  print "%15d%15d%8s%s" % (population[0][0], population[0][2], " ", population[0][1] )
+
+  bestFit = population[0][1]
+
+  while population[0][2] != 0:
+    generation += 1
+    breeder_max = int(int(options.size) / 10)
+    
+    for x in xrange(breeder_max,int(options.size)):
+      candidate = mutate( population[x%breeder_max][1] )
+      population[x] = (generation, candidate, score( candidate, target ))
+    population.sort( cmpWeasel )
+    if population[0][1] != bestFit:
+      bestFit = population[0][1]
+      print "%15d%15d%8s%s" %( population[0][0], population[0][2], " ", population[0][1])
+
+if __name__ == '__main__':
+  main()
+
