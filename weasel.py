60f40b6e7c6025cd365dcc9ca116f275a5abc6bc
John Bode <jfbode1029@gmail.com>
2017-09-28 07:53:29 -0500

    Use built-in random functions for computing die roll and picking random characters.
    Compute and display mutation rate.

--- weasel.py
+++ weasel.py
@@ -23,36 +23,36 @@ duplicate = transpose - default_duplicate
 point = duplicate - default_point
 no_change = 95
 
-def rollDie():
-  return int( random.random() * 100 )
-
-def randomChar():
-  idx = int( random.random() * len( alphabet ) )
-  return alphabet[ idx ]
-
 def randomString( slen ):
   r = []
   for i in xrange( slen ):
-    r.append( randomChar() )
+    r.append( random.choice( alphabet ))
   s = ''.join( r )
   return s
 
 def mutate( src ):
   tmp = []
+  mutCount = 0
   for i in xrange( len( src ) ):
-    action = rollDie()
+    action = random.randint(0,99)
     if action >= point and action < duplicate:
-      tmp.append( randomChar() )
+      tmp.append( random.choice( alphabet ))
+      mutCount += 1
     elif action >= duplicate and action < transpose:
       tmp.append( src[i] )
       tmp.append( src[i] )
+      mutCount += 1
     elif action >= transpose and action < delete:
       if i < len( src ) - 1:
         tmp.append( src[i+1] )
       tmp.append( src[i] )
-    elif action < point:
+      mutCount += 1
+    elif action >= delete:
+      mutCount += 1
+    else:
       tmp.append( src[i] )
-  return ''.join( tmp )
+
+  return ''.join( tmp ), float(mutCount) / float(len( src ))
 
 def cmpWeasel( w0, w1 ):
   return w0[2] - w1[2]
@@ -99,14 +99,14 @@ def main():
 
   for i in xrange(int(options.size)):
     candidate = randomString( int(options.length) )
-    population.append( (generation, candidate, score(candidate, target)) )
+    population.append( (generation, candidate, score(candidate, target), 0 ))
 
   population.sort( cmpWeasel )
 
-  print "%15s%15s%8s%s" % ("Generation", "Score", " ", "Value")
-  print "%15s%15s%8s%s" % ("----------", "-----", " ", "-----")
+  print "%15s%15s%15s%8s%s" % ("Generation", "Score", "Mut Rate", " ", "Value")
+  print "%15s%15s%15s%8s%s" % ("----------", "-----", "--------", " ", "-----")
 
-  print "%15d%15d%8s%s" % (population[0][0], population[0][2], " ", population[0][1] )
+  print "%15d%15d%15.2f%8s%s" % (population[0][0], population[0][2], population[0][3], " ", population[0][1] )
 
   bestFit = population[0][1]
 
@@ -115,12 +115,12 @@ def main():
     breeder_max = int(int(options.size) / 10)
     
     for x in xrange(breeder_max,int(options.size)):
-      candidate = mutate( population[x%breeder_max][1] )
-      population[x] = (generation, candidate, score( candidate, target ))
+      candidate, mutRate = mutate( population[x%breeder_max][1] )
+      population[x] = (generation, candidate, score( candidate, target ), mutRate)
     population.sort( cmpWeasel )
     if population[0][1] != bestFit:
       bestFit = population[0][1]
-      print "%15d%15d%8s%s" %( population[0][0], population[0][2], " ", population[0][1])
+      print "%15d%15d%15.2f%8s%s" %( population[0][0], population[0][2], population[0][3], " ", population[0][1])
 
 if __name__ == '__main__':
   main()
